baaf61cb3ca510c205242f6a931d021408345ce2
makarychev <3645723+makarychev@users.noreply.github.com>
2024-05-16 16:21:00 +0300

    add init holder, transfer rule, security associated account into tests

--- tests/solana-security-token.ts
+++ tests/solana-security-token.ts
@@ -10,23 +10,36 @@ import {
   getAssociatedTokenAddressSync,
   createAssociatedTokenAccountInstruction,
   getAccount,
+  ExtraAccountMeta,
+  createTransferCheckedInstruction,
+  createTransferCheckedWithTransferHookInstruction,
+  getExtraAccountMetaAddress,
+  getExtraAccountMetas,
+  createExecuteInstruction,
+  resolveExtraAccountMeta,
 } from "@solana/spl-token";
 import {
   Keypair,
   sendAndConfirmTransaction,
   SystemProgram,
   Transaction,
-  Connection,
+  AccountInfo,
+  PublicKey,
 } from "@solana/web3.js";
 import { TransferRestrictions } from "../target/types/transfer_restrictions";
 // import { SecurityTransferHook } from "../target/types/security_transfer_hook";
 import { assert } from "chai";
+import { topUpWallet } from "./utils";
 
 const EXTRA_METAS_ACCOUNT_PREFIX = "extra-account-metas";
-const ACCESS_CONTROL_PREFIX = "access_control";
+const ACCESS_CONTROL_PREFIX = "ac";
 const WALLET_ROLE_PREFIX = "wallet_role";
-const TRANSFER_RESTRICTION_GROUP_PREFIX = "tr_group";
-const TRANSFER_RESTRICTION_DATA_PREFIX = "transfer_restriction_data";
+const TRANSFER_RESTRICTION_GROUP_PREFIX = "trg";
+const TRANSFER_RESTRICTION_DATA_PREFIX = "trd";
+const TRANSFER_RULE_PREFIX = "tr";
+const SECURITY_ASSOCIATED_ACCOUNT_PREFIX = "saa"; // security associated account
+const TRANSFER_RESTRICTION_HOLDER_PREFIX = "trh"; // transfer_restriction_holder
+
 
 describe("solana-security-token", () => {
   const provider = anchor.AnchorProvider.env();
@@ -60,13 +73,14 @@ describe("solana-security-token", () => {
   const reserveAdmin = Keypair.generate();
   const transferAdmin = Keypair.generate();
 
+  let extraMetasAddress: PublicKey = null;
+  const validationLen = 8 + 4 + 4 + 7 * 35; // Discriminator, length, pod slice length, pod slice with 7 extra metas
+
+
   it("airdrop payer", async () => {
     console.log("Airdropping payer", superAdmin.publicKey.toString());
 
-    await provider.connection.confirmTransaction(
-      await provider.connection.requestAirdrop(superAdmin.publicKey, 1000000000000000),
-      confirmOptions
-    );
+    await topUpWallet(provider.connection, superAdmin.publicKey, 100000000000000);
   });
 
 
@@ -119,6 +133,7 @@ describe("solana-security-token", () => {
       ],
       transferRestrictionsProgram.programId
     );
+    extraMetasAddress = extraMetasAccount;
 
     const [accessControlPubkey, accessControlBump] =
       anchor.web3.PublicKey.findProgramAddressSync(
@@ -165,10 +180,11 @@ describe("solana-security-token", () => {
         .signers([mintKeypair, superAdmin])
         .rpc({ commitment: confirmOptions });
       console.log("InitializeAccessControl transaction signature", tx);
-      
+
       const accessControlData =
         await transferRestrictionsProgram.account.accessControl.fetch(
-          accessControlPubkey
+          accessControlPubkey,
+          confirmOptions
         );
       console.log("Access Control Data", accessControlData);
       assert.deepEqual(accessControlData.mint, mintKeypair.publicKey);
@@ -178,7 +194,7 @@ describe("solana-security-token", () => {
       assert.deepEqual(walletRoleData.role, 15);
 
       let mintData = await getMint(
-        connection, 
+        connection,
         mintKeypair.publicKey,
         confirmOptions,
         TOKEN_2022_PROGRAM_ID
@@ -192,30 +208,32 @@ describe("solana-security-token", () => {
       console.log("Mint Data", mintData);
 
       // 2. Mint tokens to new account
-      const newAccount = Keypair.generate();
-      const newAccountPubkey = newAccount.publicKey;
+      const userWallet = Keypair.generate();
+      const userWalletPubkey = userWallet.publicKey;
 
-      const newAssociatedAccountPubkey = getAssociatedTokenAddressSync(
-				mintKeypair.publicKey,
-				newAccountPubkey,
-				false,
-				TOKEN_2022_PROGRAM_ID
-			)
-      console.log("New Associated Account Pubkey", newAssociatedAccountPubkey.toBase58());
-      
+      const userWalletAssociatedAccountPubkey = getAssociatedTokenAddressSync(
+        mintKeypair.publicKey,
+        userWalletPubkey,
+        false,
+        TOKEN_2022_PROGRAM_ID
+      )
+      console.log("New Associated Account Pubkey", userWalletAssociatedAccountPubkey.toBase58());
 
       const transaction = new Transaction().add(
         createAssociatedTokenAccountInstruction(
           superAdmin.publicKey,
-          newAssociatedAccountPubkey,
-          newAccountPubkey,
+          userWalletAssociatedAccountPubkey,
+          userWalletPubkey,
           mintKeypair.publicKey,
           TOKEN_2022_PROGRAM_ID
         )
       )
-      const txCreateAssTokenAccount = await sendAndConfirmTransaction(provider.connection, transaction, [
-        superAdmin,
-      ]);
+      const txCreateAssTokenAccount = await sendAndConfirmTransaction(
+        provider.connection,
+        transaction,
+        [superAdmin],
+        { commitment: confirmOptions }
+      );
       console.log("Create Associated Token Account Transaction Signature", txCreateAssTokenAccount);
 
       const mintAmount = new anchor.BN(1000000);
@@ -225,8 +243,8 @@ describe("solana-security-token", () => {
           authorityWalletRole: authorityWalletRolePubkey,
           accessControl: accessControlPubkey,
           securityMint: mintKeypair.publicKey,
-          destinationAccount: newAssociatedAccountPubkey,
-          destinationAuthority: newAccountPubkey,
+          destinationAccount: userWalletAssociatedAccountPubkey,
+          destinationAuthority: userWalletPubkey,
           tokenProgram: TOKEN_2022_PROGRAM_ID,
         })
         .signers([superAdmin])
@@ -234,7 +252,7 @@ describe("solana-security-token", () => {
       console.log("Mint Securities Transaction Signature", mintTx);
 
       mintData = await getMint(
-        connection, 
+        connection,
         mintKeypair.publicKey,
         confirmOptions,
         TOKEN_2022_PROGRAM_ID
@@ -242,27 +260,27 @@ describe("solana-security-token", () => {
 
       console.log("Mint Data (after MintSecurities)", mintData);
 
-      const assAccountInfo = await getAccount(connection, newAssociatedAccountPubkey, undefined, TOKEN_2022_PROGRAM_ID);
+      const assAccountInfo = await getAccount(connection, userWalletAssociatedAccountPubkey, undefined, TOKEN_2022_PROGRAM_ID);
       console.log("Associated Account Info", assAccountInfo);
 
       // 3. Burn tokens
       const burnAmount = new anchor.BN(700000);
       const burnTx = await transferRestrictionsProgram.methods
-      .burnSecurities(burnAmount).accountsStrict({
-        authority: superAdmin.publicKey,
-        authorityWalletRole: authorityWalletRolePubkey,
-        accessControl: accessControlPubkey,
-        securityMint: mintKeypair.publicKey,
-        targetAccount: newAssociatedAccountPubkey,
-        targetAuthority: newAccountPubkey,
-        tokenProgram: TOKEN_2022_PROGRAM_ID,
-      })
-      .signers([superAdmin])
-      .rpc({ commitment: confirmOptions });
+        .burnSecurities(burnAmount).accountsStrict({
+          authority: superAdmin.publicKey,
+          authorityWalletRole: authorityWalletRolePubkey,
+          accessControl: accessControlPubkey,
+          securityMint: mintKeypair.publicKey,
+          targetAccount: userWalletAssociatedAccountPubkey,
+          targetAuthority: userWalletPubkey,
+          tokenProgram: TOKEN_2022_PROGRAM_ID,
+        })
+        .signers([superAdmin])
+        .rpc({ commitment: confirmOptions });
       console.log("Burn Securities Transaction Signature", burnTx);
 
       mintData = await getMint(
-        connection, 
+        connection,
         mintKeypair.publicKey,
         confirmOptions,
         TOKEN_2022_PROGRAM_ID
@@ -294,13 +312,13 @@ describe("solana-security-token", () => {
           tokenProgram: TOKEN_2022_PROGRAM_ID,
         })
         .signers([superAdmin])
-        .rpc({ commitment: confirmOptions }); 
+        .rpc({ commitment: confirmOptions });
       console.log("Initialize Transfer Restrictions Data Transaction Signature", initTransferRestrictionDataTx);
 
       const transferRestrictionData =
-      await transferRestrictionsProgram.account.transferRestrictionData.fetch(
-        transferRestrictionDataPubkey
-      );
+        await transferRestrictionsProgram.account.transferRestrictionData.fetch(
+          transferRestrictionDataPubkey
+        );
       console.log("transferRestrictionData:", transferRestrictionData);
       assert.deepEqual(transferRestrictionData.securityTokenMint, mintKeypair.publicKey);
       assert.deepEqual(transferRestrictionData.accessControlAccount, accessControlPubkey);
@@ -330,8 +348,246 @@ describe("solana-security-token", () => {
         .signers([superAdmin])
         .rpc({ commitment: confirmOptions });
       console.log("Initialize Transfer Restriction Group Transaction Signature", initTransferGroupTx);
+
+      // 6. Create Transfer Rule 1 -> 1
+      console.log("6. Create Transfer Rule 1 -> 1");
+      const [transferRulePubkey, transferRulePubkeyBump] =
+        anchor.web3.PublicKey.findProgramAddressSync(
+          [
+            Buffer.from(TRANSFER_RULE_PREFIX),
+            transferRestrictionGroupPubkey.toBuffer(),
+            transferRestrictionGroupPubkey.toBuffer(),
+          ],
+          transferRestrictionsProgram.programId
+        );
+      console.log("Transfer Rule Pubkey", transferRulePubkey.toBase58());
+
+      const tsNow = Date.now() / 1000;
+      const lockedUntil = new anchor.BN(tsNow);
+      const initTransferRuleTx = await transferRestrictionsProgram.methods
+        .initializeTransferRule(lockedUntil)
+        .accountsStrict({
+          transferRule: transferRulePubkey,
+          transferRestrictionData: transferRestrictionDataPubkey,
+          transferRestrictionGroupFrom: transferRestrictionGroupPubkey,
+          transferRestrictionGroupTo: transferRestrictionGroupPubkey,
+          accessControlAccount: accessControlPubkey,
+          payer: superAdmin.publicKey,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([superAdmin])
+        .rpc({ commitment: confirmOptions });
+      console.log("Initialize Transfer Rule Transaction Signature", initTransferRuleTx);
+
+      const transferRuleData = await transferRestrictionsProgram.account.transferRule.fetch(transferRulePubkey, confirmOptions)
+      console.log("Transfer Rule Data", transferRuleData);
+      assert.equal(transferRuleData.lockedUntil.toString(), lockedUntil.toString());
+      assert.equal(transferRuleData.transferGroupIdFrom.toString(), transfer_group.toString());
+      assert.equal(transferRuleData.transferGroupIdTo.toString(), transfer_group.toString());
+      assert.deepEqual(transferRuleData.transferRestrictionData, transferRestrictionDataPubkey);
+
+      // 7.0. Create security associated account from and to
+      const userWalletRecipient = Keypair.generate();
+      const userWalletRecipientPubkey = userWalletRecipient.publicKey;
+      const userWalletRecipientAssociatedTokenAccountPubkey = getAssociatedTokenAddressSync(
+        mintKeypair.publicKey,
+        userWalletRecipientPubkey,
+        false,
+        TOKEN_2022_PROGRAM_ID
+      );
+      console.log(`Recipient address: ${userWalletRecipientPubkey.toBase58()}, token account: ${userWalletRecipientAssociatedTokenAccountPubkey.toBase58()}`);
+      await topUpWallet(provider.connection, userWallet.publicKey, 1000000000000);
+      console.log(`User Wallet sender balance: ${await provider.connection.getBalance(userWalletPubkey)}`);
+
+      // 7.1. Create Holder for sender
+      console.log("7.1. Create Holder for sender");
+      const senderHolderId = new anchor.BN(1);
+      const [holderSenderPubkey] = anchor.web3.PublicKey.findProgramAddressSync(
+        [
+          Buffer.from(TRANSFER_RESTRICTION_HOLDER_PREFIX),
+          transferRestrictionDataPubkey.toBuffer(),
+          senderHolderId.toArrayLike(Buffer, "le", 8),
+        ],
+        transferRestrictionsProgram.programId
+      );
+      const initSenderHolderTx = await transferRestrictionsProgram.methods
+        .initializeTransferRestrictionHolder(senderHolderId)
+        .accountsStrict({
+          transferRestrictionHolder: holderSenderPubkey,
+          transferRestrictionData: transferRestrictionDataPubkey,
+          accessControlAccount: accessControlPubkey,
+          payer: superAdmin.publicKey,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([superAdmin])
+        .rpc({ commitment: confirmOptions });
+      console.log("Initialize Sender Holder Transaction Signature", initSenderHolderTx);
+      // 7.2. Create Holder for recipient
+      console.log("7.2. Create Holder for recipient");
+      const recipientHolderId = new anchor.BN(2);
+      const [holderRecipientPubkey] = anchor.web3.PublicKey.findProgramAddressSync(
+        [
+          Buffer.from(TRANSFER_RESTRICTION_HOLDER_PREFIX),
+          transferRestrictionDataPubkey.toBuffer(),
+          recipientHolderId.toArrayLike(Buffer, "le", 8),
+        ],
+        transferRestrictionsProgram.programId
+      );
+      const initRecipientHolderTx = await transferRestrictionsProgram.methods
+        .initializeTransferRestrictionHolder(recipientHolderId)
+        .accountsStrict({
+          transferRestrictionHolder: holderRecipientPubkey,
+          transferRestrictionData: transferRestrictionDataPubkey,
+          accessControlAccount: accessControlPubkey,
+          payer: superAdmin.publicKey,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([superAdmin])
+        .rpc({ commitment: confirmOptions });
+      console.log("Initialize Recipient Holder Transaction Signature", initRecipientHolderTx);
+      // 7.3. Create Security Associated Account for sender
+      console.log("7.3. Create Security Associated Account for sender");
+      const [userWalletSenderSecurityAssociatedTokenAccountPubkey] =
+        anchor.web3.PublicKey.findProgramAddressSync(
+          [
+            Buffer.from(SECURITY_ASSOCIATED_ACCOUNT_PREFIX),
+            userWalletAssociatedAccountPubkey.toBuffer(),
+          ],
+          transferRestrictionsProgram.programId
+        );
+      const initSecAssocAccountSenderTx = await transferRestrictionsProgram.methods
+        .initializeSecurityAssociatedAccount()
+        .accountsStrict({
+          securityAssociatedAccount: userWalletSenderSecurityAssociatedTokenAccountPubkey,
+          group: transferRestrictionGroupPubkey,
+          holder: holderSenderPubkey,
+          securityToken: mintKeypair.publicKey,
+          transferRestrictionData: transferRestrictionDataPubkey,
+          userWallet: userWalletPubkey,
+          associatedTokenAccount: userWalletAssociatedAccountPubkey,
+          payer: superAdmin.publicKey,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([superAdmin])
+        .rpc({ commitment: confirmOptions });
+
+      console.log("Initialize Security Associated Account Transaction Signature", initSecAssocAccountSenderTx);
+
+      // 7.4. Create Security Associated Account for recipient
+      console.log("7.4. Create Security Associated Account for recipient");
+      const transactionCreateAssocAccRecipient = new Transaction().add(
+        createAssociatedTokenAccountInstruction(
+          superAdmin.publicKey,
+          userWalletRecipientAssociatedTokenAccountPubkey,
+          userWalletRecipientPubkey,
+          mintKeypair.publicKey,
+          TOKEN_2022_PROGRAM_ID
+        )
+      )
+      const txCreateRecipientAssTokenAccount = await sendAndConfirmTransaction(
+        provider.connection,
+        transactionCreateAssocAccRecipient,
+        [superAdmin],
+        { commitment: confirmOptions }
+      );
+      console.log("Create Recipient Associated Token Account Transaction Signature", txCreateRecipientAssTokenAccount);
+
+      const [userWalletRecipientSecurityAssociatedTokenAccountPubkey] =
+        anchor.web3.PublicKey.findProgramAddressSync(
+          [
+            Buffer.from(SECURITY_ASSOCIATED_ACCOUNT_PREFIX),
+            userWalletRecipientAssociatedTokenAccountPubkey.toBuffer(),
+          ],
+          transferRestrictionsProgram.programId
+        );
+
+      const initSecAssocAccountRecipientTx = await transferRestrictionsProgram.methods
+        .initializeSecurityAssociatedAccount()
+        .accountsStrict({
+          securityAssociatedAccount: userWalletRecipientSecurityAssociatedTokenAccountPubkey,
+          group: transferRestrictionGroupPubkey,
+          holder: holderRecipientPubkey,
+          securityToken: mintKeypair.publicKey,
+          transferRestrictionData: transferRestrictionDataPubkey,
+          userWallet: userWalletRecipientPubkey,
+          associatedTokenAccount: userWalletRecipientAssociatedTokenAccountPubkey,
+          payer: superAdmin.publicKey,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([superAdmin])
+        .rpc({ commitment: confirmOptions });
+      console.log("Initialize Security Associated Account Transaction Signature", initSecAssocAccountRecipientTx);
+
+      // 7. Create Transfer
+      console.log("7. Create Transfer with Hook")
+      const transferAmount = BigInt(1000);
+
+      console.log("==================================== ******** ====================================");
+      console.log("==================================== FINISHED ====================================");
+      console.log("==================================== ******** ====================================");
+
     } catch (error) {
       console.log("Error", error);
     }
+  });
+
+  it("valid extra metas address", async () => {
+    // TODO: Update extra metas addresses with the correct values
+    const extraMetas: ExtraAccountMeta[] = [
+      {
+        discriminator: 0,
+        addressConfig: Keypair.generate().publicKey.toBuffer(),
+        isWritable: false,
+        isSigner: false,
+      },
+      {
+        discriminator: 0,
+        addressConfig: Keypair.generate().publicKey.toBuffer(),
+        isWritable: false,
+        isSigner: false,
+      },
+    ];
+
+    // // Check the account data
+    // const extraMetasAddressData = await provider.connection
+    //   .getAccountInfo(extraMetasAddress)
+    //   assert.equal(extraMetasAddressData.data.length, validationLen);
+    //   assert.equal(
+    //     extraMetasAddressData.data.subarray(0, 8).compare(
+    //       Buffer.from([105, 37, 101, 197, 75, 251, 102, 26]) // SPL discriminator for `Execute` from interface
+    //     ),
+    //     0
+    //   );
+    //   assert.equal(
+    //     extraMetasAddressData.data.subarray(8, 12).compare(
+    //       Buffer.from([74, 0, 0, 0]) // Little endian 74
+    //     ),
+    //     0
+    //   );
+    //   assert.equal(
+    //     extraMetasAddressData.data.subarray(12, 16).compare(
+    //       Buffer.from([2, 0, 0, 0]) // Little endian 2
+    //     ),
+    //     0
+    //   );
+    //   const extraMetaToBuffer = (extraMeta: ExtraAccountMeta) => {
+    //     const buf = Buffer.alloc(35);
+    //     buf.set(extraMeta.addressConfig, 1);
+    //     buf.writeUInt8(0, 33); // isSigner
+    //     buf.writeUInt8(0, 34); // isWritable
+    //     return buf;
+    //   };
+    //   assert.equal(
+    //     extraMetasAddressData.data
+    //       .subarray(16, 51)
+    //       .compare(extraMetaToBuffer(extraMetas[0])),
+    //     0
+    //   );
+    //   assert.equal(
+    //     extraMetasAddressData.data
+    //       .subarray(51, 86)
+    //       .compare(extraMetaToBuffer(extraMetas[1])),
+    //     0
+    //   );
   });
 });
