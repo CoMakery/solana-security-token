5ced0768374cb90ebaceab5e7d1a652da32da77a
makarychev <3645723+makarychev@users.noreply.github.com>
2024-07-30 16:51:29 +0300

    validate holder id on initialization

--- programs/transfer-restrictions/src/errors.rs
+++ programs/transfer-restrictions/src/errors.rs
@@ -20,4 +20,6 @@ pub enum TransferRestrictionsError {
     CurrentWalletsCountMustBeZero,
     #[msg("Escrow accounts mismatch")]
     EscrowAccountsMismatch,
+    #[msg("Invalid transfer restriction holder index")]
+    InvalidHolderIndex,
 }
