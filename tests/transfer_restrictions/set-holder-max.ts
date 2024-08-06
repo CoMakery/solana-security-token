bf8a88313760072a1f3cefb123e4efda062452c9
makarychev <3645723+makarychev@users.noreply.github.com>
2024-08-06 12:06:32 +0300

    add set holder max specs

---
+++ tests/transfer_restrictions/set-holder-max.ts
@@ -0,0 +1,119 @@
+import * as anchor from "@coral-xyz/anchor";
+import { assert } from "chai";
+
+import {
+  TestEnvironment,
+  TestEnvironmentParams,
+} from "../helpers/test_environment";
+
+describe("Set holder max", () => {
+  const testEnvironmentParams: TestEnvironmentParams = {
+    mint: {
+      decimals: 6,
+      name: "XYZ Token",
+      symbol: "XYZ",
+      uri: "https://e.com",
+    },
+    initialSupply: 1_000_000_000_000,
+    maxHolders: 3,
+  };
+  let testEnvironment: TestEnvironment;
+
+  before(async () => {
+    testEnvironment = new TestEnvironment(testEnvironmentParams);
+    await testEnvironment.setupAccessControl();
+    await testEnvironment.setupTransferRestrictions();
+  });
+
+  it("fails to set holder max by contract admin", async () => {
+    const signer = testEnvironment.contractAdmin;
+    const [authorityWalletRolePubkey] =
+      testEnvironment.accessControlHelper.walletRolePDA(signer.publicKey);
+    const maxHolders = new anchor.BN(10);
+    try {
+      await testEnvironment.transferRestrictionsHelper.program.methods
+        .setHolderMax(maxHolders)
+        .accountsStrict({
+          transferRestrictionData: testEnvironment.transferRestrictionsHelper.transferRestrictionDataPubkey,
+          accessControlAccount: testEnvironment.accessControlHelper.accessControlPubkey,
+          mint: testEnvironment.mintKeypair.publicKey,
+          authorityWalletRole: authorityWalletRolePubkey,
+          payer: signer.publicKey,
+        })
+        .signers([signer])
+        .rpc({ commitment: testEnvironment.commitment });
+      assert.fail("Expect an error");
+    } catch ({ error }) {
+      assert.equal(error.errorCode.code, "Unauthorized");
+      assert.equal(error.errorMessage, "Unauthorized");
+    }
+  });
+
+  it("fails to set holder max by reserve admin", async () => {
+    const signer = testEnvironment.reserveAdmin;
+    const [authorityWalletRolePubkey] =
+      testEnvironment.accessControlHelper.walletRolePDA(signer.publicKey);
+    const maxHolders = new anchor.BN(10);
+    try {
+      await testEnvironment.transferRestrictionsHelper.program.methods
+        .setHolderMax(maxHolders)
+        .accountsStrict({
+          transferRestrictionData: testEnvironment.transferRestrictionsHelper.transferRestrictionDataPubkey,
+          accessControlAccount: testEnvironment.accessControlHelper.accessControlPubkey,
+          mint: testEnvironment.mintKeypair.publicKey,
+          authorityWalletRole: authorityWalletRolePubkey,
+          payer: signer.publicKey,
+        })
+        .signers([signer])
+        .rpc({ commitment: testEnvironment.commitment });
+      assert.fail("Expect an error");
+    } catch ({ error }) {
+      assert.equal(error.errorCode.code, "Unauthorized");
+      assert.equal(error.errorMessage, "Unauthorized");
+    }
+  });
+
+  it("fails to set holder max by wallets admin", async () => {
+    const signer = testEnvironment.walletsAdmin;
+    const [authorityWalletRolePubkey] =
+      testEnvironment.accessControlHelper.walletRolePDA(signer.publicKey);
+    const maxHolders = new anchor.BN(10);
+    try {
+      await testEnvironment.transferRestrictionsHelper.program.methods
+        .setHolderMax(maxHolders)
+        .accountsStrict({
+          transferRestrictionData: testEnvironment.transferRestrictionsHelper.transferRestrictionDataPubkey,
+          accessControlAccount: testEnvironment.accessControlHelper.accessControlPubkey,
+          mint: testEnvironment.mintKeypair.publicKey,
+          authorityWalletRole: authorityWalletRolePubkey,
+          payer: signer.publicKey,
+        })
+        .signers([signer])
+        .rpc({ commitment: testEnvironment.commitment });
+      assert.fail("Expect an error");
+    } catch ({ error }) {
+      assert.equal(error.errorCode.code, "Unauthorized");
+      assert.equal(error.errorMessage, "Unauthorized");
+    }
+  });
+
+  it("sets holder group max by transfer admin", async () => {
+    const signer = testEnvironment.transferAdmin;
+    const [authorityWalletRolePubkey] =
+      testEnvironment.accessControlHelper.walletRolePDA(signer.publicKey);
+    const maxHolders = new anchor.BN(777);
+    await testEnvironment.transferRestrictionsHelper.program.methods
+      .setHolderMax(maxHolders)
+      .accountsStrict({
+        transferRestrictionData: testEnvironment.transferRestrictionsHelper.transferRestrictionDataPubkey,
+        accessControlAccount: testEnvironment.accessControlHelper.accessControlPubkey,
+        mint: testEnvironment.mintKeypair.publicKey,
+        authorityWalletRole: authorityWalletRolePubkey,
+        payer: signer.publicKey,
+      })
+      .signers([signer])
+      .rpc({ commitment: testEnvironment.commitment });
+    const groupData = await testEnvironment.transferRestrictionsHelper.transferRestrictionData();
+    assert.equal(groupData.maxHolders.toNumber(), maxHolders.toNumber());
+  });
+});
