aec6ffe0fb5280eb1f5cbbb6f251f0ad89ce4c6f
makarychev <3645723+makarychev@users.noreply.github.com>
2024-08-07 20:04:22 +0300

    add revoke holder specs

---
+++ tests/transfer_restrictions/revoke-holder.ts
@@ -0,0 +1,296 @@
+import * as anchor from "@coral-xyz/anchor";
+import { assert } from "chai";
+import { SystemProgram, Keypair } from "@solana/web3.js";
+
+import {
+  TestEnvironment,
+  TestEnvironmentParams,
+} from "../helpers/test_environment";
+
+describe("Revoke holder", () => {
+  const testEnvironmentParams: TestEnvironmentParams = {
+    mint: {
+      decimals: 6,
+      name: "XYZ Token",
+      symbol: "XYZ",
+      uri: "https://e.com",
+    },
+    initialSupply: 1_000_000_000_000,
+    maxHolders: 10,
+  };
+  let testEnvironment: TestEnvironment;
+  const groupId = new anchor.BN(1);
+  let groupPubkey: anchor.web3.PublicKey;
+
+  before(async () => {
+    testEnvironment = new TestEnvironment(testEnvironmentParams);
+    await testEnvironment.setupAccessControl();
+    await testEnvironment.setupTransferRestrictions();
+
+    const [transferAdminRole] =
+      testEnvironment.accessControlHelper.walletRolePDA(
+        testEnvironment.transferAdmin.publicKey
+      );
+    await testEnvironment.transferRestrictionsHelper.initializeTransferRestrictionGroup(
+      groupId,
+      transferAdminRole,
+      testEnvironment.transferAdmin
+    );
+    [groupPubkey] =
+      testEnvironment.transferRestrictionsHelper.groupPDA(groupId);
+    let holderIdx = new anchor.BN(0);
+    await testEnvironment.transferRestrictionsHelper.initializeTransferRestrictionHolder(
+      holderIdx,
+      transferAdminRole,
+      testEnvironment.transferAdmin
+    );
+    let [holderPubkey] =
+      testEnvironment.transferRestrictionsHelper.holderPDA(holderIdx);
+    let [holderGroupPubkey] =
+      testEnvironment.transferRestrictionsHelper.holderGroupPDA(
+        holderPubkey,
+        groupId
+      );
+    await testEnvironment.transferRestrictionsHelper.initializeHolderGroup(
+      holderGroupPubkey,
+      holderPubkey,
+      groupPubkey,
+      transferAdminRole,
+      testEnvironment.transferAdmin
+    );
+
+    holderIdx = holderIdx.addn(1);
+    await testEnvironment.transferRestrictionsHelper.initializeTransferRestrictionHolder(
+      holderIdx,
+      transferAdminRole,
+      testEnvironment.transferAdmin
+    );
+    [holderPubkey] =
+      testEnvironment.transferRestrictionsHelper.holderPDA(holderIdx);
+    [holderGroupPubkey] =
+      testEnvironment.transferRestrictionsHelper.holderGroupPDA(
+        holderPubkey,
+        groupId
+      );
+    await testEnvironment.transferRestrictionsHelper.initializeHolderGroup(
+      holderGroupPubkey,
+      holderPubkey,
+      groupPubkey,
+      transferAdminRole,
+      testEnvironment.transferAdmin
+    );
+
+    holderIdx = holderIdx.addn(1);
+    await testEnvironment.transferRestrictionsHelper.initializeTransferRestrictionHolder(
+      holderIdx,
+      transferAdminRole,
+      testEnvironment.transferAdmin
+    );
+    [holderPubkey] =
+      testEnvironment.transferRestrictionsHelper.holderPDA(holderIdx);
+    [holderGroupPubkey] =
+      testEnvironment.transferRestrictionsHelper.holderGroupPDA(
+        holderPubkey,
+        groupId
+      );
+    await testEnvironment.transferRestrictionsHelper.initializeHolderGroup(
+      holderGroupPubkey,
+      holderPubkey,
+      groupPubkey,
+      transferAdminRole,
+      testEnvironment.transferAdmin
+    );
+  });
+
+  it("fails to revoke holder by contract admin", async () => {
+    const signer = testEnvironment.contractAdmin;
+    const [authorityWalletRolePubkey] =
+      testEnvironment.accessControlHelper.walletRolePDA(signer.publicKey);
+    const [holderPubkey] = testEnvironment.transferRestrictionsHelper.holderPDA(
+      new anchor.BN(0)
+    );
+
+    try {
+      await testEnvironment.transferRestrictionsHelper.program.methods
+        .revokeHolder()
+        .accountsStrict({
+          holder: holderPubkey,
+          transferRestrictionData:
+            testEnvironment.transferRestrictionsHelper
+              .transferRestrictionDataPubkey,
+          authorityWalletRole: authorityWalletRolePubkey,
+          payer: signer.publicKey,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([signer])
+        .rpc({ commitment: testEnvironment.commitment });
+      assert.fail("Expect an error");
+    } catch ({ error }) {
+      assert.equal(error.errorCode.code, "Unauthorized");
+      assert.equal(error.errorMessage, "Unauthorized");
+    }
+  });
+
+  it("fails to revoke holder by reserve admin", async () => {
+    const signer = testEnvironment.contractAdmin;
+    const [authorityWalletRolePubkey] =
+      testEnvironment.accessControlHelper.walletRolePDA(signer.publicKey);
+    const [holderPubkey] = testEnvironment.transferRestrictionsHelper.holderPDA(
+      new anchor.BN(0)
+    );
+
+    try {
+      await testEnvironment.transferRestrictionsHelper.program.methods
+        .revokeHolder()
+        .accountsStrict({
+          holder: holderPubkey,
+          transferRestrictionData:
+            testEnvironment.transferRestrictionsHelper
+              .transferRestrictionDataPubkey,
+          authorityWalletRole: authorityWalletRolePubkey,
+          payer: signer.publicKey,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([signer])
+        .rpc({ commitment: testEnvironment.commitment });
+      assert.fail("Expect an error");
+    } catch ({ error }) {
+      assert.equal(error.errorCode.code, "Unauthorized");
+      assert.equal(error.errorMessage, "Unauthorized");
+    }
+  });
+
+  it("revokes holder by transfer admin", async () => {
+    const signer = testEnvironment.transferAdmin;
+    const [authorityWalletRolePubkey] =
+      testEnvironment.accessControlHelper.walletRolePDA(signer.publicKey);
+    const [holderPubkey] = testEnvironment.transferRestrictionsHelper.holderPDA(
+      new anchor.BN(0)
+    );
+    let accountInfo = await testEnvironment.connection.getAccountInfo(
+      holderPubkey
+    );
+    assert.isNotNull(accountInfo);
+    const { currentHoldersCount: currentHoldersCountBefore } =
+      await testEnvironment.transferRestrictionsHelper.transferRestrictionData();
+
+    await testEnvironment.transferRestrictionsHelper.program.methods
+      .revokeHolder()
+      .accountsStrict({
+        holder: holderPubkey,
+        transferRestrictionData:
+          testEnvironment.transferRestrictionsHelper
+            .transferRestrictionDataPubkey,
+        authorityWalletRole: authorityWalletRolePubkey,
+        payer: signer.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([signer])
+      .rpc({ commitment: testEnvironment.commitment });
+    accountInfo = await testEnvironment.connection.getAccountInfo(holderPubkey);
+    assert.isNull(accountInfo);
+    const { currentHoldersCount: currentHoldersCountAfter } =
+      await testEnvironment.transferRestrictionsHelper.transferRestrictionData();
+    assert.equal(
+      currentHoldersCountAfter.toNumber(),
+      currentHoldersCountBefore.toNumber() - 1
+    );
+  });
+
+  it("revokes holder by wallets admin", async () => {
+    const signer = testEnvironment.walletsAdmin;
+    const [authorityWalletRolePubkey] =
+      testEnvironment.accessControlHelper.walletRolePDA(signer.publicKey);
+    const [holderPubkey] = testEnvironment.transferRestrictionsHelper.holderPDA(
+      new anchor.BN(1)
+    );
+    let accountInfo = await testEnvironment.connection.getAccountInfo(
+      holderPubkey
+    );
+    assert.isNotNull(accountInfo);
+    const { currentHoldersCount: currentHoldersCountBefore } =
+      await testEnvironment.transferRestrictionsHelper.transferRestrictionData();
+
+    await testEnvironment.transferRestrictionsHelper.program.methods
+      .revokeHolder()
+      .accountsStrict({
+        holder: holderPubkey,
+        transferRestrictionData:
+          testEnvironment.transferRestrictionsHelper
+            .transferRestrictionDataPubkey,
+        authorityWalletRole: authorityWalletRolePubkey,
+        payer: signer.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([signer])
+      .rpc({ commitment: testEnvironment.commitment });
+    accountInfo = await testEnvironment.connection.getAccountInfo(holderPubkey);
+    assert.isNull(accountInfo);
+    const { currentHoldersCount: currentHoldersCountAfter } =
+      await testEnvironment.transferRestrictionsHelper.transferRestrictionData();
+    assert.equal(
+      currentHoldersCountAfter.toNumber(),
+      currentHoldersCountBefore.toNumber() - 1
+    );
+  });
+
+  const investor = Keypair.generate();
+  it("fails to revoke holder if some wallets are linked with it", async () => {
+    const signer = testEnvironment.transferAdmin;
+    const [authorityWalletRolePubkey] =
+      testEnvironment.accessControlHelper.walletRolePDA(signer.publicKey);
+    const [holderPubkey] = testEnvironment.transferRestrictionsHelper.holderPDA(
+      new anchor.BN(2)
+    );
+    const [holderGroupPubkey] =
+      testEnvironment.transferRestrictionsHelper.holderGroupPDA(
+        holderPubkey,
+        groupId
+      );
+    let accountInfo = await testEnvironment.connection.getAccountInfo(
+      holderGroupPubkey
+    );
+    assert.isNotNull(accountInfo);
+
+    const investorTokenAccount =
+      await testEnvironment.mintHelper.createAssociatedTokenAccount(
+        investor.publicKey,
+        testEnvironment.transferAdmin
+      );
+    await testEnvironment.transferRestrictionsHelper.initializeSecurityAssociatedAccount(
+      groupPubkey,
+      holderPubkey,
+      holderGroupPubkey,
+      investor.publicKey,
+      investorTokenAccount,
+      testEnvironment.accessControlHelper.walletRolePDA(
+        testEnvironment.walletsAdmin.publicKey
+      )[0],
+      testEnvironment.walletsAdmin
+    );
+
+    try {
+      await testEnvironment.transferRestrictionsHelper.program.methods
+        .revokeHolder()
+        .accountsStrict({
+          holder: holderPubkey,
+          transferRestrictionData:
+            testEnvironment.transferRestrictionsHelper
+              .transferRestrictionDataPubkey,
+          authorityWalletRole: authorityWalletRolePubkey,
+          payer: signer.publicKey,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([signer])
+        .rpc({ commitment: testEnvironment.commitment });
+      assert.fail("Expect an error");
+    } catch ({ error }) {
+      assert.equal(error.errorCode.code, "CurrentWalletsCountMustBeZero");
+      assert.equal(error.errorMessage, "Current wallets count must be zero");
+    }
+    accountInfo = await testEnvironment.connection.getAccountInfo(
+      holderGroupPubkey
+    );
+    assert.isNotNull(accountInfo);
+  });
+});
