15921a2ccac6ac969b1069a00623dd4cc0f79411
makarychev <3645723+makarychev@users.noreply.github.com>
2024-08-27 18:09:05 +0300

    add dividends deployment script

---
+++ deploy/new-dividends-distribution.ts
@@ -0,0 +1,114 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import BN from "bn.js";
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  clusterApiUrl,
+  Commitment,
+} from "@solana/web3.js";
+import {
+  getAccessControlProgram,
+  getDividendsProgram,
+  loadKeypairFromFile,
+} from "./helpers";
+import { AnchorProvider, Wallet } from "@coral-xyz/anchor";
+import { options } from "./commands";
+import { findDistributorKey } from "../app/src/merkle-distributor";
+import { toBytes32Array } from "../_sdk/merkle-distributor/utils";
+import { AccessControlHelper } from "../tests/helpers/access-control_helper";
+import { MintHelper } from "../tests/helpers/mint_helper";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+
+type DividendsConfig = {
+  accessControl: PublicKey;
+  securityMint: PublicKey;
+  dividendsMint: PublicKey;
+  dividendsAmount: BN;
+  rootProof: Buffer;
+  claimantsCount: BN;
+  commitment: Commitment;
+  dividendsTokenProgramId: PublicKey;
+};
+
+const config: DividendsConfig = {
+  accessControl: new PublicKey("FxpEmMdLUXHv7qmvbGeB7JEyVzkScvr1wZXR6K2nPjSy"),
+  securityMint: new PublicKey("HgbQJA9h17oJZzSpHoHRQ5zL1xugifVWgnifseAECxzn"),
+  dividendsMint: new PublicKey("HU2SxhuawUkLMznhc5Ew5Xkbm7UsEVXCNe6zWZb92sjn"),
+  dividendsAmount: new BN(10000000),
+  rootProof: Buffer.alloc(32),
+  claimantsCount: new BN(10),
+  commitment: "confirmed",
+  dividendsTokenProgramId: TOKEN_PROGRAM_ID,
+};
+const deployerKeypairPath = `deploy/${options.cluster}/keys/deployer.json`;
+
+(async () => {
+  const rpcUrl =
+    options.cluster === "localnet"
+      ? "http://localhost:8899"
+      : process.env.RPC_URL || clusterApiUrl(options.cluster);
+  const commitment = config.commitment as Commitment;
+
+  const deployerKeypair = loadKeypairFromFile(deployerKeypairPath);
+  const wallet = new Wallet(deployerKeypair);
+
+  const connection = new Connection(rpcUrl, commitment);
+  const provider = new AnchorProvider(connection, wallet, { commitment });
+  const dividendsProgram = getDividendsProgram(provider);
+  const accessControlProgram = getAccessControlProgram(provider);
+
+  const baseKey = Keypair.generate();
+  const [distributor, bump] = findDistributorKey(
+    baseKey.publicKey,
+    dividendsProgram.programId
+  );
+
+  const accessControlHelper = new AccessControlHelper(
+    accessControlProgram,
+    config.securityMint,
+    commitment
+  );
+  console.log(`Configuration is initialized. Creating distributor...`);
+
+  await dividendsProgram.methods
+    .newDistributor(
+      bump,
+      toBytes32Array(config.rootProof),
+      config.dividendsAmount,
+      config.claimantsCount
+    )
+    .accountsStrict({
+      base: baseKey.publicKey,
+      distributor,
+      mint: config.dividendsMint,
+      authorityWalletRole: accessControlHelper.walletRolePDA(
+        deployerKeypair.publicKey
+      )[0],
+      accessControl: accessControlHelper.accessControlPubkey,
+      payer: deployerKeypair.publicKey,
+      systemProgram: SystemProgram.programId,
+    })
+    .signers([deployerKeypair, baseKey])
+    .rpc({ commitment });
+  console.log(`Distributor created: ${distributor.toBase58()}`);
+
+  const distributorData =
+    await dividendsProgram.account.merkleDistributor.fetch(distributor);
+  console.log(`Distributor data: ${JSON.stringify(distributorData)}`);
+
+  const dividendsMintHelper = new MintHelper(
+    connection,
+    config.dividendsMint,
+    config.commitment,
+    config.dividendsTokenProgramId
+  );
+  const distributorATA = await dividendsMintHelper.createAssociatedTokenAccount(
+    distributor,
+    deployerKeypair,
+    true
+  );
+  console.log(`Distributor ATA created: ${distributorATA.toBase58()}`);
+})();
